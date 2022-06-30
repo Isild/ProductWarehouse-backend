4954097c2b390bbda3794e3c7c9fd95399c66077
Isild <pimok96@gmail.com>
2022-07-01 01:03:24 +0200

    add data casting in model

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -23,6 +23,8 @@ class ProductModel extends Model
      */
     protected $casts = [
         'salary' => 'array',
+        'created_at'    => 'datetime:Y-m-d H:i:s',
+        'updated_at'    => 'datetime:Y-m-d H:i:s',
     ];
 
     /**
